a71e305a800f26c4e4632facddddd4c3353caace
Gnanavel <gnanavelpandi123@gmail.com>
2021-06-10 14:23:14 +0530

    Timer Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,10 +60,20 @@ $(document).ready(function(){
         });
     })
 })
-var count = new Date('dec 31,2020 00:00:00').getTime();
+var count = new Date('dec 31,2040 00:00:00').getTime();
 var x = setInterval(function(){
+    
     var now = new Date().getTime();
     var d = count - now;
+    if(d<=0){
+        document.getElementById('weeks').innerHTML = 0;
+        document.getElementById('days').innerHTML = 0;
+        document.getElementById('hours').innerHTML = 0;
+        document.getElementById('minutes').innerHTML = 0;
+        document.getElementById('seconds').innerHTML = 0;
+        clearInterval(x);
+        return
+    }
     var weeks = Math.floor(d/(1000*60*60*24*7));
     var days = Math.floor(d/(1000*60*60*24));
     var hours = Math.floor((d%(1000*60*60*24))/(1000*60*60));
@@ -74,9 +84,7 @@ var x = setInterval(function(){
     document.getElementById('hours').innerHTML = hours;
     document.getElementById('minutes').innerHTML = minutes;
     document.getElementById('seconds').innerHTML = seconds;
-    if(d<=0){
-        clearInterval(x);
-    }
+    
 },1000);
 var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
